f4b47682a3cf422afcfd31d41615bcf96a22c8cb
Patrick O'Grady <ogradypatrickj@gmail.com>
2015-01-28 10:04:53 -0500

    #2 - add HTML pages
    -Add jquery-ui
    -Add custom UI skin CSS
    -Add my_personal_calendar page
    -Add my_personal_calendar_members page
    -Add my_personal_tags page
    -Add home page
    -Add settings page
    -Add create_team_personal page
    -Add onboarding pages
    
    -add controller tests for new pages
    
    -configure Hound, Rubocop & Guard
    
    -update ruby version in circle.yml

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,4 +12,40 @@ class PagesController < ApplicationController
     render
   end
 
+  def my_personal_calendar
+    render
+  end
+
+  def my_personal_calendar_members
+    render
+  end
+
+  def my_personal_calendar_tags
+    render
+  end
+
+  def home
+    render
+  end
+
+  def settings
+    render
+  end
+
+  def create_team_personal
+    render
+  end
+
+  def onboarding_1
+    render
+  end
+
+  def onboarding_create_team
+    render
+  end
+
+  def onboarding_invite_members
+    render
+  end
+
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class PagesControllerTest < ActionController::TestCase
 
@@ -16,6 +16,60 @@ class PagesControllerTest < ActionController::TestCase
 
   def test_about_success
     get :about
+
+    assert_response :success
+  end
+
+  def test_my_personal_calendar_success
+    get :my_personal_calendar
+
+    assert_response :success
+  end
+
+  def test_my_personal_calendar_members_success
+    get :my_personal_calendar_members
+
+    assert_response :success
+  end
+
+  def test_my_personal_calendar_tags_success
+    get :my_personal_calendar_tags
+
+    assert_response :success
+  end
+
+  def test_home_success
+    get :home
+
+    assert_response :success
+  end
+
+  def test_settings_success
+    get :settings
+
+    assert_response :success
+  end
+
+  def test_create_team_personal_success
+    get :create_team_personal
+
+    assert_response :success
+  end
+
+  def test_onboarding_1_success
+    get :onboarding_1
+
+    assert_response :success
+  end
+
+  def test_onboarding_create_team_success
+    get :onboarding_create_team
+
+    assert_response :success
+  end
+
+  def test_onboarding_invite_members_success
+    get :onboarding_invite_members
 
     assert_response :success
   end
